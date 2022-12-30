dbcfefbd40995ae375ced84348014e609532f491
agbuenoch <agbuenoch38@gmail.com>
2022-12-30 14:39:33 +0000

    Hello world commit

--- 0x06-python-classes/102-main.py
+++ 0x06-python-classes/102-main.py
@@ -6,13 +6,18 @@ s_6 = Square(6)
 
 if s_5 < s_6:
     print("Square 5 < Square 6")
+
 if s_5 <= s_6:
     print("Square 5 <= Square 6")
+    
 if s_5 == s_6:
     print("Square 5 == Square 6")
+    
 if s_5 != s_6:
     print("Square 5 != Square 6")
+    
 if s_5 > s_6:
     print("Square 5 > Square 6")
+    
 if s_5 >= s_6:
     print("Square 5 >= Square 6")
