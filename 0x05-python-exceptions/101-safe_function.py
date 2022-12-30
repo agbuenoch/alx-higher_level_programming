4982c2cb2852c310519e9f48aafba4bd5ad96e9a
agbuenoch <agbuenoch38@gmail.com>
2022-12-30 12:18:33 +0000

    Hello world commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
 
-    """Executes a function safely.
-
+    """
+    Executes a function safely.
     Args:
         fct: The function to execute.
         args: Arguments for fct.
