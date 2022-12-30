043f71464be989bc37e89a2c68680ddabf415adb
agbuenoch <agbuenoch38@gmail.com>
2022-12-30 14:11:53 +0000

    Hello world commit

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -14,7 +14,7 @@ class Square:
         self.size = size
         self.position = position
         
-        @property
+    @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
