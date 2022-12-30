46e0895a04a8d0270c227f3801f4e8a631012d0f
agbuenoch <agbuenoch38@gmail.com>
2022-12-30 14:16:00 +0000

    Hello world commit

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -18,7 +18,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,12 +26,12 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
-    
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
@@ -40,7 +40,7 @@ class Square:
             not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-        
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
@@ -56,7 +56,7 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-            
+
     def __str__(self):
         """Define the print() representation of a Square."""
         if self.__size != 0:
